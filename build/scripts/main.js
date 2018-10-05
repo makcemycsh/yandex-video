80a4657e3a518e14606c40434c21bd0ee28feba8
Шилов Максим <m.shilo.v80@gmail.com>
2018-10-05 08:42:07 +0300

    fixed header

--- build/scripts/main.js
+++ build/scripts/main.js
@@ -1,2 +1,2 @@
-"use strict";$(".js-menu-bar").on("click",function(){$(this).toggleClass("is-active")});
+"use strict";$(document).ready(function(){$(window).scroll(function(){150<$(this).scrollTop()?($("body").addClass("head-is-fixed"),$(".head-is-fixed").css("margin-top",$(".js-head").outerHeight(!0))):($(".head-is-fixed").css("margin-top",0),$("body").removeClass("head-is-fixed"))}),$(".js-menu-bar").on("click",function(){$(this).toggleClass("is-active")})});
 //# sourceMappingURL=main.js.map
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
-$('.js-menu-bar').on('click', function () {
-  $(this).toggleClass('is-active');
-});
+$(document).ready(function () {
+// Фиксация хедера при скролле
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 150) {
+      $('body').addClass('head-is-fixed');
+      $('.head-is-fixed').css('margin-top', $('.js-head').outerHeight(true));
+    } else {
+      $('.head-is-fixed').css('margin-top', 0);
+      $('body').removeClass('head-is-fixed');
+    }
+  });
+
+//Выпадающее меню
+  $('.js-menu-bar').on('click', function () {
+    $(this).toggleClass('is-active');
+  });
+})
